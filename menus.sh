05f40ad0f1f1cf79c72203b5cbe236878812f2a0
Steven Adel <stevenadel.dev@gmail.com>
2023-12-16 20:32:07 +0200

    Add secondary (table) menu

--- menus.sh
+++ menus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 main_menu() {
+    PS3="bashDB>> "
     echo -e "\nSelect database operation:"
     select choice in 'Create Database' 'List Databases' 'Connect to Database' 'Drop Database' 'Exit'
     do
@@ -17,6 +18,35 @@ main_menu() {
         ;;
         *) echo "Please choose from available options 1-5."
            main_menu
+        ;; 
+        esac
+    done
+}
+
+table_menu() {
+    echo -e "\nSelect table operation:"
+    select choice in 'Create Table' 'List Table' 'Select from Table' 'Insert into Table' 'Delete from Table' 'Update Table' 'Drop Table' "Exit Table $1"
+    do
+        case $REPLY in
+        1) create_table
+        ;;
+        2) list_table
+        ;;
+        3) select_table
+        ;;
+        4) insert_table
+        ;;
+        5) delete_table
+        ;;
+        6) update_table
+        ;;
+        7) drop_table
+        ;;
+        8)  cd ..
+            main_menu
+        ;;
+        *) echo "Please choose from available options 1-8."
+           table_menu
         ;; 
         esac
     done
