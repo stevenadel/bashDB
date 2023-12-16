caebd692e1647a79faaf4c1dd5fffefde77c959b
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2023-12-16 13:11:33 +0200

    create_database.sh

--- create_database.sh
+++ create_database.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# still under work
-function create_database() {
-    if [[ ! -d ./bashDB ]]
-    then
-        mkdir bashDB
-    fi
+function create_database {
+    while true; do
+        read -p "Please enter database name: " db_name
+        # Check for spaces in the name
+        if [[ $db_name = *" "* ]]; then
+            echo "Name of the database can't contain spaces!"
+            db_name=$(echo ${db_name// /_})
+             echo "New name: $db_name"
+        # Check if the name is empty
+        elif [ ! "$db_name" ]; then
+            echo "You didn't enter a database name"
+        # Check for special characters or numbers in the name
+        elif [[ ! "$db_name" =~ ^[a-zA-Z]*$ ]]; then
+            echo "Name of the database can't contain special characters or numbers!"
+        # Check if the database already exists
+        elif [ -e "./ourdatabase/$db_name" ]; then
+            echo "Database already exists!"
+        else
+            break
+        fi
+
+        read -p "Please enter database name: " name
+    done
     
-    cd ./bashDB
-    read -p "Please enter database name: " db_name
-    # TODO validate_name
-
-    if [[ -d "$db_name" ]]
-    then
-        echo "Database already exists!"
-    else
-        mkdir "$db_name"
-    fi
+    mkdir -p "./ourdatabase/$db_name"
+    echo "Database created successfully"
 }
-
-create_database
