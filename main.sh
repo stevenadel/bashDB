26643d0162a7d026e4f634eb94e79b604ebd6634
Steven Adel <stevenadel.dev@gmail.com>
2023-12-17 21:07:43 +0200

    main

--- main.sh
+++ main.sh
@@ -4,13 +4,13 @@ source ./utils_database/create_database.sh
 source ./utils_database/list_database.sh
 source ./utils_database/connect_database.sh
 source ./utils_database/drop_database.sh
-source ./utils_table/create_table.sh
+#source ./utils_table/create_table.sh
 source ./utils_table/list_table.sh
 source ./utils_table/select_table.sh
-source ./utils_table/insert_table.sh
-source ./utils_table/delete_table.sh
-source ./utils_table/update_table.sh
-source ./utils_table/drop_table.sh
+#source ./utils_table/insert_table.sh
+#source ./utils_table/delete_table.sh
+#source ./utils_table/update_table.sh
+#source ./utils_table/drop_table.sh
 source ./utils_table/data_type_validate.sh
 
 if [[ ! -d ./bashDB ]]
@@ -19,8 +19,8 @@ then
 fi
 cd bashDB
 
-echo "###############################"
-echo "------ Welcome to bashDB ------"
-echo "###############################"
+echo "###################################"
+echo "-------- Welcome to bashDB --------"
+echo "###################################"
  
 main_menu
